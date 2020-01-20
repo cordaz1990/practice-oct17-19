9c4ef666288fa2c4c95a72e66a43fc04865fe4bc
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 14:37:35 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,20 @@
-print("hello")
-print("world")
-print("hello")
-print()
+from typing import TextIO
+from io import StringIO
+import time_series
+
+def find_largest(line: str) -> int:
+    """ Return the largest value in line, which is a whitespce-delimited string of integers that each end with a '.'.
+    
+    >>> find_largest('1. 3. 2. 5. 2.')
+    5
+    """ 
+    #The largest value seen so far
+    lrgest = -1
+    for value in line.split():
+       #Remove the trailing period.
+        v = int(value[:-1])
+        #If we find a larger value, remember it.
+        if v > largest:
+            largest = v
+     
+     return largest
